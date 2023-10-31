5ee1805c9c099928c10be059531a338ee155e3fd
poorin tinanop <poorinbboy@gmail.com>
2023-10-31 19:20:08 +0700

    Add new file

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -161,12 +161,12 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="text-white">
+            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="text-yellow-500 text-xl">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
         <div class="pt-2 pb-3 space-y-1 mx-3">
-            <x-nav-link href="{{ route('soldier') }}" :active="request()->routeIs('soldier')"  class="text-yellow-500 ">
+            <x-nav-link href="{{ route('soldier') }}" :active="request()->routeIs('soldier')"  class="text-yellow-500 text-xl">
                 {{ __('ข้อมูลกำลังพล') }}
              </x-nav-link>
 
