c4f1a85e68a5fd49a37f6eef445346d18fb452cd
poorin tinanop <poorinbboy@gmail.com>
2023-11-08 20:44:55 +0700

    Add new file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
         <link rel="icon" type="image/x-icon" href="/image/logo/favicon.ico">
 
-        <title>Armyunit</title>
+        <title>DataUnit</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
