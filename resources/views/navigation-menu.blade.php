e85d0e6df0bac3420b5450cce81e6328eb56e82a
poorin tinanop <poorinbboy@gmail.com>
2023-11-30 19:42:47 +0700

    Add new file

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -11,7 +11,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:block md:flex lg:flex xl:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:block md:block lg:flex xl:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="text-white ">
                         {{ __('Dashboard') }}
                     </x-nav-link>
