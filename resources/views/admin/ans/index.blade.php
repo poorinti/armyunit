dad29fda320bc2723aa2eb86625c725bdbb20b48
poorin tinanop <poorinbboy@gmail.com>
2023-12-19 15:21:40 +0700

    Add new file

--- resources/views/admin/ans/index.blade.php
+++ resources/views/admin/ans/index.blade.php
@@ -143,7 +143,7 @@
                                     @enderror
                                     @php
                                         $iArr= array();
-                                        $iArr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+                                        $iArr=[01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                                     @endphp
                                     <label for="ans_index">เรียงลำดับ</label>
                                     <select class=" form-control form-select" aria-label="ans_index" name="ans_index" required>
